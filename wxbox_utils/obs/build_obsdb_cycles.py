80c61414fd0ed2b6f477b9e36e1b678022b492be
jongmantari <jong@mantari.com>
2026-08-11 18:53:34 -0400

    add patch tempest obs stationelev

--- wxbox_utils/obs/build_obsdb_cycles.py
+++ wxbox_utils/obs/build_obsdb_cycles.py
@@ -20,208 +20,85 @@ else:
 with open(cfgfile) as f:
     cfg = yaml.safe_load(f)
 
-start = dt.datetime.strptime(
-    cfg["cycles"]["start"],
-    "%Y%m%dT%HZ"
-)
+start = dt.datetime.strptime(cfg["cycles"]["start"], "%Y%m%dT%HZ")
+end = dt.datetime.strptime(cfg["cycles"]["end"], "%Y%m%dT%HZ")
 
-end = dt.datetime.strptime(
-    cfg["cycles"]["end"],
-    "%Y%m%dT%HZ"
-)
-
-cycle_step = dt.timedelta(
-    hours=cfg["cycles"]["interval_hours"]
-)
+cycle_step = dt.timedelta(hours=cfg["cycles"]["interval_hours"])
 
 before_hours = cfg["window"]["before_hours"]
 after_hours = cfg["window"]["after_hours"]
 
-outroot = pathlib.Path(
-    cfg["output"]["root"]
-)
+outroot = pathlib.Path(cfg["output"]["root"])
 
 KEEP_GROUPS = [
     "MetaData",
     "ObsValue",
     "DerivedObsValue",
-    "PreQC"
+    "PreQC",
 ]
 
-# =====================================================
-# HELPERS
-# =====================================================
+FILL_FLOAT = np.float32(-3.3688e38)
+
 
 def copy_global_attributes(src, dst):
-
     for att in src.ncattrs():
+        dst.setncattr(att, src.getncattr(att))
 
-        dst.setncattr(
-            att,
-            src.getncattr(att)
-        )
-
-
-# =====================================================
-# MAIN
-# =====================================================
 
 for obs_type in cfg["obs_types"]:
 
     obs_cfg = cfg["obs"][obs_type]
 
-    input_dir = pathlib.Path(
-        obs_cfg["input_dir"]
-    )
-
-    file_pattern = (
-        obs_cfg["file_pattern"]
-    )
-
-    output_pattern = (
-        obs_cfg["output_pattern"]
-    )
-
-    obs_errors = (
-        obs_cfg["obserror"]
-    )
+    input_dir = pathlib.Path(obs_cfg["input_dir"])
+    file_pattern = obs_cfg["file_pattern"]
+    output_pattern = obs_cfg["output_pattern"]
+    obs_errors = obs_cfg["obserror"]
 
     current = start
 
     while current <= end:
 
-        cycle_dir = current.strftime(
-            "%Y%m%dT%HZ"
-        )
+        cycle_dir = current.strftime("%Y%m%dT%HZ")
+        outfile_name = current.strftime(output_pattern)
 
-        outfile_name = current.strftime(
-            output_pattern
-        )
+        outdir = outroot / obs_type / cycle_dir
+        outdir.mkdir(parents=True, exist_ok=True)
 
-        outdir = (
-            outroot
-            / obs_type
-            / cycle_dir
-        )
-
-        outdir.mkdir(
-            parents=True,
-            exist_ok=True
-        )
-
-        outfile = (
-            outdir
-            / outfile_name
-        )
-
-        # -------------------------------------------------
-        # gather files in window
-        # -------------------------------------------------
+        outfile = outdir / outfile_name
 
         infiles = []
 
-        for dh in range(
-            -before_hours,
-            after_hours + 1
-        ):
-
-            t = (
-                current
-                + dt.timedelta(
-                    hours=dh
-                )
-            )
-
-            fname = t.strftime(
-                file_pattern
-            )
-
-            fpath = (
-                input_dir
-                / fname
-            )
-
+        for dh in range(-before_hours, after_hours + 1):
+            t = current + dt.timedelta(hours=dh)
+            fpath = input_dir / t.strftime(file_pattern)
             if fpath.exists():
-
-                infiles.append(
-                    fpath
-                )
+                infiles.append(fpath)
 
         if len(infiles) == 0:
-
-            print(
-                "No files:",
-                cycle_dir
-            )
-
+            print('No files:', cycle_dir)
             current += cycle_step
             continue
 
-        print()
-        print(
-            "Cycle:",
-            cycle_dir
-        )
+        print() 
+        print('Cycle:', cycle_dir)
+        print('Files:', len(infiles))
 
-        print(
-            "Files:",
-            len(infiles)
-        )
-
-        # -------------------------------------------------
-        # sample file
-        # -------------------------------------------------
-
-        sample = nc.Dataset(
-            infiles[0]
-        )
+        sample = nc.Dataset(infiles[0])
 
         total_locations = 0
-
         for f in infiles:
-
             ds = nc.Dataset(f)
-
-            total_locations += len(
-                ds.dimensions[
-                    "Location"
-                ]
-            )
-
+            total_locations += len(ds.dimensions['Location'])
             ds.close()
 
-        # -------------------------------------------------
-        # create output
-        # -------------------------------------------------
+        out = nc.Dataset(outfile, 'w')
 
-        out = nc.Dataset(
-            outfile,
-            "w"
-        )
+        copy_global_attributes(sample, out)
 
-        copy_global_attributes(
-            sample,
-            out
-        )
+        out.createDimension('Location', total_locations)
 
-        out.createDimension(
-            "Location",
-            total_locations
-        )
-
-        locvar = out.createVariable(
-            "Location",
-            "i8",
-            ("Location",)
-        )
-
-        locvar[:] = np.arange(
-            total_locations
-        )
-
-        # -------------------------------------------------
-        # create schema
-        # -------------------------------------------------
+        locvar = out.createVariable('Location', 'i8', ('Location',))
+        locvar[:] = np.arange(total_locations)
 
         for gname in KEEP_GROUPS:
 
@@ -229,93 +106,41 @@ for obs_type in cfg["obs_types"]:
                 continue
 
             gsrc = sample.groups[gname]
-
-            gout = out.createGroup(
-                gname
-            )
+            gout = out.createGroup(gname)
 
             for vname in gsrc.variables:
 
-                srcvar = (
-                    gsrc.variables[
-                        vname
-                    ]
-                )
-
+                srcvar = gsrc.variables[vname]
                 dtype = srcvar.dtype
 
                 fill_value = None
+                if '_FillValue' in srcvar.ncattrs():
+                    fill_value = srcvar.getncattr('_FillValue')
 
-                if (
-                    "_FillValue"
-                    in srcvar.ncattrs()
-                ):
-                    fill_value = (
-                        srcvar.getncattr(
-                            "_FillValue"
-                        )
-                    )
-
-                if (
-                    str(dtype)
-                    .startswith(
-                        "<class 'str'"
-                    )
-                ):
-
-                    outvar = (
-                        gout.createVariable(
-                            vname,
-                            str,
-                            ("Location",)
-                        )
-                    )
-
+                if str(dtype).startswith("<class 'str'"):
+                    outvar = gout.createVariable(vname, str, ('Location',))
                 else:
-
-                    outvar = (
-                        gout.createVariable(
-                            vname,
-                            dtype,
-                            ("Location",),
-                            fill_value=fill_value
-                        )
+                    outvar = gout.createVariable(
+                        vname,
+                        dtype,
+                        ('Location',),
+                        fill_value=fill_value,
                     )
 
-                for att in (
-                    srcvar.ncattrs()
-                ):
-
-                    if att == "_FillValue":
+                for att in srcvar.ncattrs():
+                    if att == '_FillValue':
                         continue
-
-                    outvar.setncattr(
-                        att,
-                        srcvar.getncattr(att)
-                    )
+                    outvar.setncattr(att, srcvar.getncattr(att))
 
         sample.close()
-
-        # -------------------------------------------------
-        # concatenate
-        # -------------------------------------------------
 
         offset = 0
 
         for f in infiles:
 
             ds = nc.Dataset(f)
-
-            nloc = len(
-                ds.dimensions[
-                    "Location"
-                ]
-            )
-
-            sl = slice(
-                offset,
-                offset + nloc
-            )
+            nloc = len(ds.dimensions['Location'])
+            sl = slice(offset, offset + nloc)
 
             for gname in KEEP_GROUPS:
 
@@ -326,101 +151,99 @@ for obs_type in cfg["obs_types"]:
                 gout = out.groups[gname]
 
                 for vname in gout.variables:
-
-                    if (
-                        vname
-                        not in
-                        gsrc.variables
-                    ):
+                    if vname not in gsrc.variables:
                         continue
-
-                    gout.variables[
-                        vname
-                    ][sl] = (
-                        gsrc.variables[
-                            vname
-                        ][:]
-                    )
+                    gout.variables[vname][sl] = gsrc.variables[vname][:]
 
             offset += nloc
-
             ds.close()
 
         # -------------------------------------------------
-        # ObsError
+        # Metadata patch for Tempest-style observations
         # -------------------------------------------------
 
-        if (
-            "ObsError"
-            not in out.groups
-        ):
-            out.createGroup(
-                "ObsError"
-            )
+        if 'MetaData' in out.groups:
 
-        gerr = out.groups[
-            "ObsError"
-        ]
-
-        for varname, err in (
-            obs_errors.items()
-        ):
+            gmeta = out.groups['MetaData']
+            nloc = total_locations
 
             if (
-                "ObsValue"
-                not in out.groups
+                'height' in gmeta.variables
+                and 'stationElevation' not in gmeta.variables
             ):
-                continue
 
-            if (
-                varname
-                not in
-                out.groups[
-                    "ObsValue"
-                ].variables
-            ):
-                continue
-
-            v = gerr.createVariable(
-                varname,
-                np.float32,
-                ("Location",)
-            )
-
-            v[:] = np.full(
-                total_locations,
-                err,
-                dtype=np.float32
-            )
-
-            try:
-
-                v.units = (
-                    out.groups[
-                        "ObsValue"
-                    ]
-                    .variables[
-                        varname
-                    ]
-                    .units
+                elev = gmeta.createVariable(
+                    'stationElevation',
+                    np.float32,
+                    ('Location',),
+                    fill_value=FILL_FLOAT,
                 )
 
+                elev.units = 'm'
+                elev.long_name = 'station elevation above mean sea level'
+
+                elev[:] = gmeta.variables['height'][:] - 2.0
+
+                print('Patched MetaData/stationElevation')
+
+            if 'sensorThermoHeight' not in gmeta.variables:
+
+                v = gmeta.createVariable(
+                    'sensorThermoHeight',
+                    np.float32,
+                    ('Location',),
+                    fill_value=FILL_FLOAT,
+                )
+
+                v.units = 'm'
+                v.long_name = 'thermometer height above local ground'
+                v[:] = np.full(nloc, 2.0, dtype=np.float32)
+
+                print('Patched MetaData/sensorThermoHeight')
+
+            if 'sensorMomentumHeight' not in gmeta.variables:
+
+                v = gmeta.createVariable(
+                    'sensorMomentumHeight',
+                    np.float32,
+                    ('Location',),
+                    fill_value=FILL_FLOAT,
+                )
+
+                v.units = 'm'
+                v.long_name = 'anemometer height above local ground'
+                v[:] = np.full(nloc, 10.0, dtype=np.float32)
+
+                print('Patched MetaData/sensorMomentumHeight')
+
+        if 'ObsError' not in out.groups:
+            out.createGroup('ObsError')
+
+        gerr = out.groups['ObsError']
+
+        for varname, err in obs_errors.items():
+
+            if 'ObsValue' not in out.groups:
+                continue
+
+            if varname not in out.groups['ObsValue'].variables:
+                continue
+
+            v = gerr.createVariable(varname, np.float32, ('Location',))
+            v[:] = np.full(total_locations, err, dtype=np.float32)
+
+            try:
+                v.units = out.groups['ObsValue'].variables[varname].units
             except Exception:
                 pass
 
-            v.long_name = (
-                "observation error "
-                f"for {varname}"
-            )
+            v.long_name = f'observation error for {varname}'
 
         out.close()
 
-        print(
-            "Created:",
-            outfile
-        )
+        print('Created:', outfile)
 
         current += cycle_step
 
-print()
-print("DONE")
+print() 
+print('DONE')
